b2662f324752d0d4e33a51c60b438137881da406
Murad <muradrashidov995@gmail.com>
2023-04-30 07:51:18 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+// tailwind.config.js
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+      screens:{
+        sm:"640px",
+        md:"768px",
+        lg:"1024px",
+        xl:"1280px",
+        "2xl":"1536px"
+
+      }
+    },
+    
+
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
